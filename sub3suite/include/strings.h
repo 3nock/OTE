36129c3f9ee449d7f89cddb3db743454127bcb88
Enock <3nock@protonmail.com>
2023-06-23 17:19:46 +0300

    removed previous sub3suite project source

--- sub3suite/include/strings.h
+++
@@ -1,6 +0,0 @@
-/*Dummy file to satisfy source file dependencies on Windows platform*/
-#if defined(_WIN32)
-	#define strcasecmp _stricmp
-	#define strncasecmp _strnicmp
-	#define inline __inline
-#endif
