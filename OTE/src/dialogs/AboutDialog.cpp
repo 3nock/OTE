7c36ee3693f0beabe3819cd4e41076ec8d2017c3
Enock <3nock@protonmail.com>
2023-07-16 23:57:02 +0300

    changed about description

--- OTE/src/dialogs/AboutDialog.cpp
+++ OTE/src/dialogs/AboutDialog.cpp
@@ -23,14 +23,25 @@ AboutDialog::AboutDialog(QWidget *parent) :
     ui->labelAbout->setTextFormat(Qt::RichText);
     ui->labelAbout->setTextInteractionFlags(Qt::TextBrowserInteraction);
     ui->labelAbout->setOpenExternalLinks(true);
-
-    // setting up description
-    QFile file(":/files/res/files/about.html");
-    if(file.open(QIODevice::ReadOnly|QIODevice::Text))
-    {
-        ui->labelAbout->setText(file.readAll());
-        file.close();
-    }
+	ui->labelAbout-->setText(R"(<h1>OSINT Template Engine</h1>
+Version 1.0.0
+<br>
+<br>
+© Copyright OSINT Template Engine 2023
+<br>
+<br>
+<a href="https://github.com/3nock/OTE">OSINT Template Engine</a> is a research-grade information gathering tool.
+<br>
+<br>
+Visit <a href=https://spidersuite.github.io/OTE>OSINT Template Engine</a> for more information and Follow <a href="https://twitter.com/intent/follow?screen_name=spider_suite&tw_p=followbutton">@spider_suite</a> for updates on the project's progress.
+<br>
+<br>
+This product includes software developed by the following open source projects:<br>
+  • SQLite database library: <a href="https://sqlite.org">https://sqlite.org</a><br>
+<br>
+<br>
+Want to help? <a href="https://spidersuite.github.io/sponsor/">Make a donation</a> or <a href="https://github.com/3nock/OTE/blob/main/CONTRIBUTING.md">get involved!</a><br>
+)");
 }
 
 AboutDialog::~AboutDialog()
